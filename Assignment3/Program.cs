060cfe0715bd59cb7c2e09b8cdfe2165b0a3a4db
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-07-20 23:24:31 -0400

    Changed Console.WriteLine message

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -6,7 +6,7 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Team!");
         }
     }
 }
