4c7b65aae2ff5d8e23ffdceb1bdf14bed16c326c
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-07-24 19:46:58 -0400

    Almost done with Account class

--- Assignment3/Account.cs
+++ Assignment3/Account.cs
@@ -13,5 +13,49 @@ namespace Assignment3
         public double Balance { get; protected set; }
         public double LowestBalance { get; protected set; }
         public string Number { get; }
+        public Account(string type, double balance)
+        {
+            Number = type + LAST_NUMBER;
+            ++LAST_NUMBER;
+            Balance = balance;
+            LowestBalance = balance;
+        }
+        public void Deposit(double amount, Person person)
+        {
+            /*public Transaction( string accountNumber, double amount, Person person, DateTime time )*/
+            Balance += amount;
+            LowestBalance = LowestBalance <= Balance ? LowestBalance : Balance;
+            transactions.Add(new Transaction(Number, amount, person, DateTime.Now));
+        }
+        public void AddUser(Person person)
+        {
+            users.Add(person);
+        }
+        public bool IsUser(string name)
+        {
+            foreach (Person user in users)
+            {
+                if(user.Name == name)
+                {
+                    return true;
+                } 
+            }
+            return false;
+        }
+        public abstract void PrepareMonthlyReport();
+        public override string ToString()
+        {
+            string representation = "";
+            foreach (Person user in users)
+            {
+                representation += "Account number: " + Number + "\nName: " + user.Name
+                    + "\nBalance: " + Balance + "\nTransactions: \n";
+                foreach(Transaction transaction in transactions)
+                {
+                    representation += transaction + "\n";
+                }
+            }
+            return representation;
+        }
     }
 }
