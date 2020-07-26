8c034a23109f2ad524b57f4f7a3b5cab46c8c065
Aakriti Gupta <draakritig@gmail.com>
2020-07-26 18:09:21 -0400

    SavingAccount updated

--- Assignment3/SavingAccount.cs
+++ Assignment3/SavingAccount.cs
@@ -11,22 +11,38 @@ namespace Assignment3
         private static double INTEREST_RATE =0.015;
 
         //CONSTRUCTOR
-        public SavingAccount(double balance = 0)
-        {
-            
-        }
+        public SavingAccount(double balance = 0) : base("SV-", balance){}
         //METHODS
         public void Deposit(double amount, Person person)
         {
-            
+            base.Deposit(amount, person);
         }
         public void Withdraw(double amount , Person person)
-        { 
-        
+        {
+            foreach (Person user in Bank.GetAccount(Number).users)
+            {
+                if (user.SIN != person.SIN || user.Name != person.Name)
+                {
+                    throw new AccountException(ExceptionEnum.NAME_NOT_ASSOCIATED_WITH_ACCOUNT.ToString());
+                }
+            }
+            if (!(person.IsAuthenticated))
+            {
+                throw new AccountException(ExceptionEnum.USER_NOT_LOGGED_IN.ToString());
+            }
+            if (amount > Balance)
+            {
+                throw new AccountException(ExceptionEnum.CREDIT_LIMIT_HAS_BEEN_EXCEEDED.ToString());
+            }
+            base.Deposit(-amount, person);
         }
-        public void PrepareMonthlyReport(double amount, Person person)
-        { 
-          
+        public override void PrepareMonthlyReport()
+        {
+            double serviceCharge = transactions.Count * COST_PER_TRANSACTION;
+            double interest = (LowestBalance * INTEREST_RATE) / 12;
+            Balance += interest;
+            Balance -= serviceCharge;
+            transactions.Clear();
         }
     }
 }
