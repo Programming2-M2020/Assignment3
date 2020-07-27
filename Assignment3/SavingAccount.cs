b4f24155a262560ea90ce083024723ed40a26027
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-07-27 12:54:44 -0400

    Enhancements to Checking and saving

--- Assignment3/SavingAccount.cs
+++ Assignment3/SavingAccount.cs
@@ -19,16 +19,10 @@ namespace Assignment3
         }
         public void Withdraw(double amount , Person person)
         {
-            bool toVerify = false;
-            foreach (Person user in Bank.GetAccount(Number).users)
+            if (!IsUser(person.Name))
             {
-                if (user.SIN == person.SIN || user.Name == person.Name)
-                {
-                    toVerify = true;
-                    break;
-                }
+                throw new AccountException(ExceptionEnum.NAME_NOT_ASSOCIATED_WITH_ACCOUNT.ToDescriptionString());
             }
-            _ = toVerify ? toVerify : throw new AccountException(ExceptionEnum.NAME_NOT_ASSOCIATED_WITH_ACCOUNT.ToDescriptionString());
             if (!(person.IsAuthenticated))
             {
                 throw new AccountException(ExceptionEnum.USER_NOT_LOGGED_IN.ToDescriptionString());
